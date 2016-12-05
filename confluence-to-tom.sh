4da168da78650d103d2f75e3b5ea84d15c1a79b2
root <root@surface.tomneilly.com>
2016-12-04 20:44:24 -0500

    delete extra atlassian image

--- confluence-to-tom.sh
+++ confluence-to-tom.sh
@@ -2,4 +2,5 @@
 
 for i in `ls`;
 do sudo sed -i '/<link rel="stylesheet" href="styles\/site.css" type="text\/css" \/>/a <link rel="stylesheet" href="styles\/custom.css" type="text\/css" \/>' $i;
+sudo sed -i '/<div id="footer-logo"><a href="http:\/\/www.atlassian.com\/">Atlassian<\/a><\/div>/d' $i;
 done;
